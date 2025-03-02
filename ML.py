e47d9da1dcc3ddc6f02d961676e270c17d006cbb
MBridwell <masonbridwellcollege@gmail.com>
2025-03-02 17:11:39 -0500

    WORKING MODEL LETS GO

--- ML.py
+++ ML.py
@@ -7,26 +7,38 @@ from tensorflow.keras.layers import Dense  # type: ignore
 from tensorflow.keras.preprocessing.image import ImageDataGenerator  # type: ignore
 import cv2 # type:ignore
 import os
+import datetime
+import matplotlib.pyplot as plt
 
 #height and width of which the pictures get formatted to
-img_height = 256
-img_width = 256
+img_height = 128
+img_width = 128
 
 #amount of images to be tested for each pass during training and validation
-batch_size = 20
+batch_size = 40
 
 #actual cnn model -> taken from example, will need worked on to be more effecient
 CNNModel = Sequential([
-    layers.Input((256, 256, 1)),
-    layers.Conv2D(16, 3, padding='same'),
-    layers.Conv2D(32, 3, padding='same'),
-    layers.MaxPooling2D(),
+    layers.Input((img_height, img_width, 3)),
+    layers.Conv2D(32, (3,3), padding='same', activation='relu'),
+    layers.MaxPooling2D(pool_size = (2,2), strides = (2,2)),
+
+    layers.Conv2D(64, (3,3), padding='same', activation='relu'),
+    layers.MaxPooling2D(pool_size = (2,2), strides = (2,2)),
+
+    layers.Conv2D(128, (3,3), padding='same', activation='relu'),
+    layers.MaxPooling2D(pool_size = (2,2), strides = (2,2)),
+
     layers.Flatten(),
+    layers.Dense(128, activation = 'relu'),
+    layers.Dropout(0.5),
     layers.Dense(1, activation = 'sigmoid'),
+
+    
 ])
 
 
-
+#datasets
 #import training set from kaggle
 # Import custom dataset
 ds_train = tf.keras.preprocessing.image_dataset_from_directory(
@@ -34,14 +46,12 @@ ds_train = tf.keras.preprocessing.image_dataset_from_directory(
     r'C:\Users\mason\Desktop\Dataset\dataset\dataset\train',  
     labels='inferred',
     label_mode="int",
-    color_mode='grayscale',
+    color_mode='rgb',
     batch_size=batch_size,
-    class_names=['0', '1'],  # Fixed comma
+    class_names=['0', '1'], 
     image_size=(img_height, img_width),
     shuffle=True,
 )
-  
-print(ds_train)
 
 #import validation set from kaggle
 ds_validate = tf.keras.preprocessing.image_dataset_from_directory(
@@ -49,79 +59,113 @@ ds_validate = tf.keras.preprocessing.image_dataset_from_directory(
     r'C:\Users\mason\Desktop\Dataset\dataset\dataset\validate',  
     labels='inferred',
     label_mode="int",
-    color_mode='grayscale',
+    color_mode='rgb',
     batch_size=batch_size,
     class_names=['0', '1'], 
     image_size=(img_height, img_width),
     shuffle=True,
     
 )
+
+ds_test = tf.keras.preprocessing.image_dataset_from_directory(
+    r'C:\Users\mason\Desktop\Dataset\dataset\dataset\test',  
+    labels='inferred',
+    label_mode="int",
+    color_mode='rgb',
+    batch_size=batch_size,
+    class_names=['0', '1'], 
+    image_size=(img_height, img_width),
+    shuffle=False,
+)
+
+
+
 print(ds_validate)
 
 
 ##compile the model. Loss and optimizer will need to be changed to improve results
-#CNNModel.compile(optimizer='adam',
-#                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),  
-#                  metrics=['accuracy'])
-#
-###print the history of the model
-#history = CNNModel.fit(ds_train, epochs=2, 
-#                    validation_data=(ds_validate))
-#
-## save a model as pkl file
-#with open('cnn_model_revision_2.pkl', 'wb') as f:
-#   p.dump(CNNModel, f)
-#
-#
-#print("Model saved as:  cnn_model1.pk_revision_2")
+def compile():
+    CNNModel.compile(optimizer='Adam',
+                      loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),  
+                      metrics=[
+                        tf.keras.metrics.FalseNegatives(),
+                        tf.keras.metrics.FalsePositives(),
+                        tf.keras.metrics.TruePositives(),
+                        tf.keras.metrics.TrueNegatives(),
+                        'accuracy'
+                        ])
+    #
+    #print the history of the model
+    history = CNNModel.fit(ds_train, epochs=5, 
+                        validation_data=(ds_validate))
+    #
+    ## save a model as pkl file
+    with open('cnn_model_revision_6.pkl', 'wb') as f:
+       p.dump(CNNModel, f)
 
 
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.plot(history.history['false_negatives'], label = 'False negatives')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0, 1])
+    plt.legend(loc='lower right')
+    plt.show()
+    #
+    #
+    print("Model saved as:  cnn_model_revision_6")
+    
+
+def test(model_name):
 #to load and test model after:
-with open('cnn_model_revision_2.pkl', 'rb') as f:
-    loaded_model = p.load(f)
+    with open(model_name, 'rb') as f:
+        loaded_model = p.load(f)
 
-predictions = CNNModel.predict(ds_validate.take(10))
-print(predictions)
+   #test_loss, false_negatives, test_accuracy, false_positives = loaded_model.evaluate(ds_test)
 
-##Load the model
-print("Model Loaded!")
-##test image path
-image_path = 'test.jpg'
-##read the image
-image = cv2.imread(image_path)
-##resize the image
-resized_image = cv2.resize(image,(256, 256))
-##convert it to black and white
-resize2bw = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
-##normalize the image
-normalize = resize2bw / 255.0
-##add color channel
-image_with_channel = np.expand_dims(normalize, axis=-1)
-##add tensor size
-image_input = np.expand_dims(image_with_channel, axis=0)
-##predict
-predict = loaded_model.predict(image_input)
-##predict logic
+   #print("Test Loss: ", test_loss)
+   #print("Test Accuracy: ", test_accuracy)
+   #print("False Negatives: ", false_negatives)
+   #print("False Positives: ", false_positives)
 
-##print the prediction (Unsure if working)
-print(predict)
+    image_path = 'Test2.png'
+
+    image = cv2.imread(image_path)
+    resized = cv2.resize(image, (128,128))
+    image_with_channel = np.expand_dims(resized, axis=-1)
+    image_dim = np.expand_dims(image_with_channel, axis=0)
+    prediction = loaded_model.predict(image_dim)
+    print(prediction)
+
+
+
+    
+   # ####Load the model
+   # print("Model Loaded!")
+   # ####test image path
+   # image_path = 'Image.png'
+   # ####read the image
+   # image = cv2.imread(image_path)
+   # ####resize the image
+   # resized_image = cv2.resize(image,(512, 512))
+   # ####normalize the image
+   # normalize = resized_image / 255.0
+   # ####add color channel
+   # image_with_channel = np.expand_dims(normalize, axis=-1)
+   # ####add tensor size
+   # image_input = np.expand_dims(image_with_channel, axis=0)
+   # ##
+   # ####print the prediction (Unsure if working)
+   # prediction = loaded_model.predict(image_input)
 #
+   # if prediction[0] >=0.5:
+   #     print("Model predicts image to be fake")
+   # else:
+   #     print("Model predicts image to be real")
+
+
+    #print("Raw Prediction:", prediction)
 #
-##Test
-#
-#
-##TODO:
-# see why model is predicting extremely high, overfitting issue, misclassifications
-# #   Create directory of images to be tested splicer.py can do this, but needs work
-# #   Should return a series of images that are a specific resolution that have faces detected with opencv2
-# #   pseudocode: 
-# #  For i in test set
-##   get image to test
-## perform processing on image
-## predict from trained model (#print(CNNModel.predict(imagedata)
-## if model thinks it is fake, add fake counter
-## if model thinks it is real, add real counter
-## calculate percentage certainty off of counters, return that value as "final guess" (whichever counter is greater, percentage certainty)
-## Opencv2 in theory isn't entirely 100% helpful to identify faces that are ai generated, so there should be a percentage loss as well incase there are false positives.
-## How can i pull faces another way?
-#
+# compile()
+test('cnn_model_revision_6.pkl')
